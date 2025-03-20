71b9b637ee79eae8190ba979bd91e3526073b1ec
Aashrya77 <aashryasigdel0@gmail.com>
2025-03-20 20:41:59 +0545

    design-impv

--- App/src/Components/CTA1/CTA1.jsx
+++ App/src/Components/CTA1/CTA1.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./CTA1.css";
+import { Link } from 'react-router-dom';
+
 const CTA1 = () => {
   return (
     <div className="CTA1">
       <div className="img-overlay"></div>
       <div className="cta1Texts">
         <h1>STACK & SAVE</h1>
-        <h4>Checkout some serious bundles with large savings!</h4>
-        <button>Shop Now</button>
+        <p>Checkout some serious bundles with large savings!</p>
+        <Link to="/shop-all" className="shop-now-btn">Shop Now</Link>
       </div>
     </div>
   );
