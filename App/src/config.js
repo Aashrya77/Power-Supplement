e4eeeae45bab8bcc9bc382091adc8d96cd5470a7
Aashrya77 <aashrya77@gmail.com>
2025-05-31 14:04:16 +0545

    Keep only dist folder for frontend deployment

--- App/src/config.js
+++
@@ -1,9 +0,0 @@
-// API Configuration
-// export const API_URL = "https://power-supplement.onrender.com";
-// const BASE_URL = "https://power-supplement.onrender.com";
-export const API_URL = "http://localhost:5500";
-const BASE_URL = "http://localhost:5500";
-
-
-
-export default BASE_URL;
