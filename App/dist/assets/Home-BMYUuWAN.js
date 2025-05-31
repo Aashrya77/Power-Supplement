4c2ed45cda27d8bc6986551e973157c362abb866
Aashrya77 <aashrya77@gmail.com>
2025-05-31 14:01:10 +0545

    Add dist folder for frontend deployment

---
+++ App/dist/assets/Home-BMYUuWAN.js
@@ -0,0 +1,2 @@
+import{j as m}from"./index-DcckHycf.js";import"./vendor-MgaVaktg.js";import"./ui-BzfpwB1b.js";const t=()=>m.jsx("div",{className:"home",children:m.jsx("img",{src:"\\Home.png",alt:""})});export{t as default};
+//# sourceMappingURL=Home-BMYUuWAN.js.map
