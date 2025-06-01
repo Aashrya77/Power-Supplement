62a09a6c52204b365718a2acc82f16e7cc0728e4
Aashrya77 <aashrya77@gmail.com>
2025-06-01 20:57:50 +0545

    https

--- App/src/config.js
+++ App/src/config.js
@@ -1,6 +1,6 @@
 // API Configuration
-export const API_URL = "http://powersupplement.net";
-const BASE_URL = "http://powersupplement.net";
+export const API_URL = "https://powersupplement.net";
+const BASE_URL = "https://powersupplement.net";
 // export const API_URL = "http://localhost:5500";
 // const BASE_URL = "http://localhost:5500";
 
