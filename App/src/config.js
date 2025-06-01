1584a51de4c70e8cc7faeec3d6f0c1b0809737a4
Aashrya77 <aashrya77@gmail.com>
2025-06-01 14:56:28 +0545

    base-url

--- App/src/config.js
+++ App/src/config.js
@@ -1,8 +1,8 @@
 // API Configuration
-// export const API_URL = "https://power-supplement.onrender.com";
-// const BASE_URL = "https://power-supplement.onrender.com";
-export const API_URL = "http://localhost:5500";
-const BASE_URL = "http://localhost:5500";
+export const API_URL = "http://powersupplement.net";
+const BASE_URL = "http://powersupplement.net";
+// export const API_URL = "http://localhost:5500";
+// const BASE_URL = "http://localhost:5500";
 
 
 
