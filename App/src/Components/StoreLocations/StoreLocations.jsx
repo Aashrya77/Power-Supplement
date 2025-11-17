d05a4fe92380cd27c16e7dfa28b72c6344f6c6fa
Aashrya <aashryasigdel0@gmail.com>
2025-11-17 12:14:46 +0545

    store

--- App/src/Components/StoreLocations/StoreLocations.jsx
+++ App/src/Components/StoreLocations/StoreLocations.jsx
@@ -7,7 +7,7 @@ const StoreLocations = () => {
   const stores = [
     {
       id: 1,
-      name: "Power Supplement - Kathmandu (Main Branch)",
+      name: "Power Supplement - Kathmandu",
       location: "New Road, Kathmandu, Nepal",
       phone: "+977-9851345820",
       hours: "9:00 AM - 8:00 PM",
@@ -16,13 +16,21 @@ const StoreLocations = () => {
     {
       id: 2,
       name: "Rs Supplement",
-      location: "Kamal Pokhari, Kathamandu, Nepal",
-      phone: "+977-9842222220",
+      location: "Kumari Party, Lalitpur, Nepal",
+      phone: "+977-9851345820",
       hours: "9:00 AM - 8:00 PM",
-      mapLink: "https://maps.google.com"
+      mapLink: "https://www.google.com/maps/place/Rs+Supplement/@27.6721953,85.2978155,15z/data=!3m1!4b1!4m6!3m5!1s0x39eb1900606e3d8d:0x916bc89f0d6cd0ad!8m2!3d27.6721775!4d85.3162696!16s%2Fg%2F11xw27xnbm?entry=ttu&g_ep=EgoyMDI1MTExMi4wIKXMDSoASAFQAw%3D%3D"
     },
     {
       id: 3,
+      name: "Rs Supplement",
+      location: "Kamal Pokhari, Kathamandu, Nepal",
+      phone: "+977-9842222220",
+      hours: "9:00 AM - 8:00 PM",
+      mapLink: "https://www.google.com/maps/place/RS+Supplement/@27.7121496,85.3237819,17z/data=!3m1!4b1!4m6!3m5!1s0x39eb19a93a925823:0x4e9e919d7bda187b!8m2!3d27.7121449!4d85.3263568!16s%2Fg%2F11sq5kt8bf?entry=ttu&g_ep=EgoyMDI1MTExMi4wIKXMDSoASAFQAw%3D%3Dhttps://www.google.com/maps/place/RS+Supplement/@27.7121496,85.3237819,17z/data=!3m1!4b1!4m6!3m5!1s0x39eb19a93a925823:0x4e9e919d7bda187b!8m2!3d27.7121449!4d85.3263568!16s%2Fg%2F11sq5kt8bf?entry=ttu&g_ep=EgoyMDI1MTExMi4wIKXMDSoASAFQAw%3D%3D"
+    },
+    {
+      id: 4,
       name: "Rise Fitness by Kamal",
       location: "Jawalakhel, Pokhara, Nepal",
       phone: "+977-9801234569",
@@ -30,15 +38,15 @@ const StoreLocations = () => {
       mapLink: "https://maps.google.com"
     },
     {
-      id: 4,
+      id: 5,
       name: "Rs Supplement",
       location: "Damak, Jhapa, Nepal",
       phone: "+977-9801234570",
       hours: "9:00 AM - 8:00 PM",
-      mapLink: "https://maps.google.com"
+      mapLink: "https://www.google.com/maps/place/Rs+supplements/@26.6598621,87.6974959,17z/data=!3m1!4b1!4m6!3m5!1s0x39e591a3dded343d:0xb6a1429f6d204cfa!8m2!3d26.6598573!4d87.7000708!16s%2Fg%2F11kpl51hvb?entry=ttu&g_ep=EgoyMDI1MTExMi4wIKXMDSoASAFQAw%3D%3D"
     },
     {
-      id: 5,
+      id: 6,
       name: "Shrestha Supplement",
       location: "Chitwan, Nepal",
       phone: "+977-9845049280",
@@ -46,8 +54,8 @@ const StoreLocations = () => {
       mapLink: "https://maps.google.com"
     },
     {
-      id: 6,
-      name: "Inbigtus Fitness - Chitwan",
+      id: 7,
+      name: "Invictus Fitness - Chitwan",
       location: "Jadibuti, Kathamndu, Nepal",
       phone: "+977-9801234572",
       hours: "9:00 AM - 8:00 PM",
